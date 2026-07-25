401053daff69ba30ca311db85fdd3ead738f8964
fozerc <an07ka@gmail.com>
2026-07-25 22:16:15 +0200

    first commit

---
+++ build_makemore.py
@@ -0,0 +1,9 @@
+words = open('names.txt', 'r').read().splitlines()
+
+b = {}
+for w in words[:3]:
+    chs = ['<S>'] + list(w) + ['<E>']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        b[bigram] = b.get(bigram, 0) + 1
+        print(ch1, ch2)
