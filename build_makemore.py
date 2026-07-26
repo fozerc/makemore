9eff547a01c4dea0d7e630110e810ace219cc55e
fozerc <an07ka@gmail.com>
2026-07-26 23:46:22 +0200

    torch

--- build_makemore.py
+++ build_makemore.py
@@ -1,9 +1,15 @@
+import torch
 words = open('names.txt', 'r').read().splitlines()
 
-b = {}
+N = torch.zeros((27, 27), dtype=torch.int32)
+chars = sorted(set(''.join(words)))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
+
 for w in words[:3]:
-    chs = ['<S>'] + list(w) + ['<E>']
+    chs = ['.'] + list(w) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
-        bigram = (ch1, ch2)
-        b[bigram] = b.get(bigram, 0) + 1
-        print(ch1, ch2)
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
