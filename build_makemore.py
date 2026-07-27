a64d8e74e7a806209698cc42da480e27fbab1318
fozerc <an07ka@gmail.com>
2026-07-27 14:28:42 +0200

    visualization

--- build_makemore.py
+++ build_makemore.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib.pyplot as plt
+
 words = open('names.txt', 'r').read().splitlines()
 
 N = torch.zeros((27, 27), dtype=torch.int32)
@@ -7,9 +9,19 @@ stoi = {s:i+1 for i,s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
 
-for w in words[:3]:
+for w in words:
     chs = ['.'] + list(w) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
         N[ix1, ix2] += 1
+
+plt.figure(figsize=(16,16))
+plt.imshow(N, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha='center', va='bottom', color='grey')
+        plt.text(j, i, N[i, j].item(), ha='center', va='top', color='grey')
+plt.axis('off')
+plt.show()
