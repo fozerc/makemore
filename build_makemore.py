173d4a7b4324289df5a19099de0bccae833ce8be
fozerc <an07ka@gmail.com>
2026-07-27 16:47:48 +0200

    neg log likelihood

--- build_makemore.py
+++ build_makemore.py
@@ -26,7 +26,7 @@ for w in words:
 # plt.axis('off')
 # plt.show()
 
-P = N.float()
+P = (N+1).float()
 P = P / P.sum(1, keepdim=True)
 
 g = torch.Generator().manual_seed(2147483647)
@@ -40,3 +40,20 @@ for i in range(50):
         if ix == 0:
             break
     print(''.join(out))
+
+log_likelihood = 0.0
+n = 0
+
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        prob = P[ix1, ix2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        n += 1
+
+nll = -log_likelihood
+print(nll)
+print(nll/n)
