4b5c97c8f26396565aee85ef60bc00a42b1afc36
fozerc <an07ka@gmail.com>
2026-08-03 11:08:32 +0200

    fix

--- build_makemore_mlp.py
+++ build_makemore_mlp.py
@@ -4,25 +4,27 @@ import torch.nn.functional as F
 
 words = open('names.txt', 'r').read().splitlines()
 
-chars = sorted(list(set(words)))
+chars = sorted(list(set(''.join(words))))
 stoi = {s:i+1 for i,s in enumerate(chars)}
 stoi['.'] = 0
-itos = {i;s for i,s in stoi.items()}
+itos = {i:s for s,i in stoi.items()}
 
-block_size = 10
+block_size = 3
 X, Y = [], []
 
-for w in words[5:]:
-    print(w)
+for w in words[:5]:
     context = [0] * block_size
     for ch in w + '.':
         ix = stoi[ch]
         X.append(context)
         Y.append(ix)
         print(''.join(itos[i] for i in context), '--->', itos[ix])
-        context = context[1:] + ix
+        context = context[1:] + [ix]
 
 X = torch.tensor(X)
 Y = torch.tensor(Y)
+print(X.shape, Y.shape)
 
 C = torch.randn(27, 2)
+emb = C[X]
+print(emb.shape)
